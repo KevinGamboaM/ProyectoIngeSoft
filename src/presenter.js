8ace796f6b7e7a4ca4f89e724da54eb2bfe57d11
vale_rivero002 <valeria.rivero@ucb.edu.bo>
2024-04-29 14:44:30 -0400

    Agregar un form para agregar proyecto

--- src/presenter.js
+++ src/presenter.js
@@ -1,1 +1,21 @@
-//
+import { agregarProyecto } from "./tdd";
+
+const modal = document.getElementById('modal');
+const agregarProyectoBtn = document.getElementById('agregar-proyecto');
+const cerrarModalBtn = document.getElementsByClassName('close')[0];
+const agregarProyectoForm = document.getElementById('agregar-proyecto-form');
+const proyectosLista = document.getElementById('proyectos-lista'); 
+agregarProyectoBtn.onclick = function() {
+  modal.style.display = 'block';
+}
+
+
+cerrarModalBtn.onclick = function() {
+  modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
