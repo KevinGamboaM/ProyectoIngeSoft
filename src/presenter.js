45ed302e89fcf4fcf36dc19545541e27a75e0032
vale_rivero002 <valeria.rivero@ucb.edu.bo>
2024-04-29 14:46:17 -0400

    Agregar estilos generales

--- src/presenter.js
+++ src/presenter.js
@@ -5,6 +5,7 @@ const agregarProyectoBtn = document.getElementById('agregar-proyecto');
 const cerrarModalBtn = document.getElementsByClassName('close')[0];
 const agregarProyectoForm = document.getElementById('agregar-proyecto-form');
 const proyectosLista = document.getElementById('proyectos-lista'); 
+
 agregarProyectoBtn.onclick = function() {
   modal.style.display = 'block';
 }
