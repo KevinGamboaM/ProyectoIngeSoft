332b68a61f212dc160bbbc43a0d0b73ecb547c20
vale_rivero002 <valeria.rivero@ucb.edu.bo>
2024-04-30 10:15:52 -0400

    Mejorar funcion AgregarProyecto para mejorar visivilidad

--- src/tdd.js
+++ src/tdd.js
@@ -1,5 +1,22 @@
 export function agregarProyecto(nombre, descripcion, listaProyectos) {
   const nuevoProyecto = document.createElement('li');
   nuevoProyecto.textContent = `${nombre}: ${descripcion}`;
-  listaProyectos.appendChild(nuevoProyecto);
+  const contenedorProyecto = document.createElement('div');
+  contenedorProyecto.classList.add('proyecto');
+
+  // Crear el título del proyecto
+  const tituloProyecto = document.createElement('h3');
+  tituloProyecto.textContent = nombre+ ": ";
+
+  // Crear la descripción del proyecto
+  const descripcionProyecto = document.createElement('p');
+  descripcionProyecto.textContent = descripcion;
+
+
+  // Agregar elementos al contenedor del proyecto (caja)
+  contenedorProyecto.appendChild(tituloProyecto);
+  contenedorProyecto.appendChild(descripcionProyecto);
+
+  // Agregar el contenedor del proyecto a la lista de proyectos
+  listaProyectos.appendChild(contenedorProyecto);
 }
